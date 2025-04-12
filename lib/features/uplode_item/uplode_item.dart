6141ddedfb2c15be3b11b8c1bcbbf27f3b247848
AhmedYasserTaha <ayt614614@gmail.com>
2025-04-12 22:47:33 +0200

    create uplode item body

--- lib/features/uplode_item/uplode_item.dart
+++ lib/features/uplode_item/uplode_item.dart
@@ -1,69 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:recycle_app/core/service/style.dart';
+import 'package:recycle_app/features/uplode_item/widget/uplode_item_body.dart';
 
 class UplodeItem extends StatelessWidget {
   const UplodeItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.arrow_back, size: 30),
-        title: Text("Uplode Item", style: Styles.textStyle25),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 213, 213, 249),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-              ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 30),
-                    child: Container(
-                      height: 150,
-                      width: 150,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black45, width: 2),
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.white,
-                      ),
-                      child: Icon(Icons.camera_alt_rounded, size: 40),
-                    ),
-                  ),
-                  Text(
-                    textAlign: TextAlign.center,
-                    "Enter your address you want the\nitem to be picked",
-                    style: Styles.textStyle25,
-                  ),
-                  SizedBox(height: 20),
-
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    margin: EdgeInsets.symmetric(horizontal: 30),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black45, width: 2),
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.white,
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(border: InputBorder.none),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    return UplodeItemBody();
   }
 }
