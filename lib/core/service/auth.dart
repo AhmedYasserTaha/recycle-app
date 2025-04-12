9405cbe319c0d22caa425c422ac96ae052cb3681
AhmedYasserTaha <ayt614614@gmail.com>
2025-04-12 18:16:11 +0200

    update mian

--- lib/core/service/auth.dart
+++ lib/core/service/auth.dart
@@ -44,9 +44,11 @@ class Auth {
 
       await DatabaseMehtods().addUserInfo(userInfoMap, user.uid);
       Navigator.pushReplacement(
+        // ignore: use_build_context_synchronously
         context,
         MaterialPageRoute(builder: (context) => HomePage()),
       );
     }
+    return null;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:recycle_app/features/auth/login/login_page.dart';
 import 'package:recycle_app/features/uplode_item/uplode_item.dart';
 
 void main() async {
